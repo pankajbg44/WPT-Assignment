5c83ecbea8e6969f3d570dd85218aa39f3e6d39c
pankajbg44 <pankajbg44@gmail.com>
2023-05-13 07:55:03 +0530

    5

---
+++ WPT-Day5/fontcolor.js
@@ -0,0 +1,24 @@
+function applyColor(c) {
+    let ptag = document.querySelector("p");
+    ptag.style.color = c;
+  }
+
+  function applyColorDiv(d) {
+    let ptag = document.querySelector("div");
+    ptag.style.color = d;
+  }
+  
+  function applyFont(a) {
+    let ptag = document.querySelector("p");
+    ptag.style.fontSize = a;
+  }
+
+  function applyFontDiv(f) {
+    let ptag = document.querySelector("#p1");
+    ptag.style.fontSize = f;
+  }
+
+  function applyFontp1(f) {
+    let ptag = document.querySelector("div");
+    ptag.style.fontSize = f;
+  }
